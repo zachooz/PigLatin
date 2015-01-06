e819f8171f0c17bd723e905df671ee87315bd0b8
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2015-01-05 17:05:35 -0800

    Finished

--- build-tmp/source/PigLatin.java
+++ build-tmp/source/PigLatin.java
@@ -1,4 +1,22 @@
-import java.util.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class PigLatin extends PApplet {
+
+
 
 public void setup() {
 	String lines[] = loadStrings("words.txt");
@@ -34,4 +52,13 @@ public String pigLatin(String sWord){
 	} else {
 		return "ERROR";
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "PigLatin" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
