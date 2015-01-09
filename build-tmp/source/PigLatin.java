78b46f95fb5393308624e81db366694778b76ffc
zachooz <hoffking@gmail.com>
2015-01-09 10:12:13 -0800

    hymn translated

--- build-tmp/source/PigLatin.java
+++ build-tmp/source/PigLatin.java
@@ -43,17 +43,30 @@ public int findFirstVowel(String sWord){
 }
 
 public String pigLatin(String sWord){
+	Boolean isUpperCase = !sWord.equals(sWord.toLowerCase());
+	if(isUpperCase){
+		sWord = sWord.toLowerCase();
+	}
 	if(sWord.equals("") || sWord.equals(" ")){
 		return "";
 	}
 	if(findFirstVowel(sWord) == -1){
+		if(isUpperCase)
+			return sWord.substring(0, 1).toUpperCase() + sWord.substring(1) + "ay";
 		return sWord + "ay";
 	} else if(findFirstVowel(sWord) == 0){
+		if(isUpperCase)
+			return sWord.substring(0, 1).toUpperCase() + sWord.substring(1) + "way";
 		return sWord + "way";
-	} else if(findFirstVowel(sWord)>0){
-		return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0,findFirstVowel(sWord)) + "ay";
-	} else if(findFirstVowel(sWord) > 0){
-		return sWord.substring(1) + sWord.substring(0,1) + "ay";
+	} else if(sWord.length() > 3 && sWord.substring(0,2).equals("qu")) {
+		return sWord.substring(2) + "quay";
+	}else if(findFirstVowel(sWord)>0){
+		String output = sWord.substring(findFirstVowel(sWord)) + sWord.substring(0,findFirstVowel(sWord)) + "ay";
+		if(isUpperCase){
+			return output.substring(0, 1).toUpperCase() + output.substring(1);
+		} else {
+			return output;
+		}
 	} else {
 		return "ERROR";
 	}
@@ -71,7 +84,6 @@ public String lowellHymDecoder(String hym){
 		} else if(hym.substring(i, i+1).equals(",")){
 			//System.out.println(hym.substring(i, i+1));
 			output+=pigLatin(hym.substring(start, finish));
-			System.out.println(hym.substring(start, finish));
 			//output = output.substring(0, output.length()-1);
 			output+=", ";
 			start = finish+1;
